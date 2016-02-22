8fa2eee2d609e576daaa6b875d3519230441b033
Kostas Vogias <vogias@admin.grnet.gr>
2016-02-22 14:15:20 +0200

    ClassObjectCreation constructor made private

--- src/main/java/ke/entry/ClassObjectCreation.java
+++ src/main/java/ke/entry/ClassObjectCreation.java
@@ -2,9 +2,13 @@ package ke.entry;
 
 public class ClassObjectCreation {
 
+	private ClassObjectCreation() {
+
+	}
+
 	public static Object createClassObject(String classname)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-		
+
 		ClassLoader classLoader = ClassLoader.getSystemClassLoader();
 		Class<?> class2Load;
 
